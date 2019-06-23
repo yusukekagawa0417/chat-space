e248e7536e0bf8cae585b84e107363efe93fec1f
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-06-23 17:58:18 +0900

    update
    ルートパスの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'messages#index'
+  root to: 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:edit, :upload, :new, :create] do
     resources :messages, only: [:index, :create]
