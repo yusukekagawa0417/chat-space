aa3e4bdfaf6cb80a2e520fbc84815da1c9760068
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-06-30 14:08:36 +0900

    prepare API
    rooting、controller修正
    jbuilder作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :index]  
   resources :groups, only: [:edit, :update, :new, :create] do
     resources :messages, only: [:index, :create]
   end
